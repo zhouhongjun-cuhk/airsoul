d5d7b1e759b1e27304318529d6426e5cd06864c3
cassel <z709316540@gmail.com>
2025-04-18 10:57:13 +0800

    remove test file

--- projects/OmniRL/run.sh
+++
@@ -1,4 +0,0 @@
-export CUDA_VISIBLE_DEVICES=7
-export PYTHONPATH="$PYTHONPATH: /home/wangfan/cassel/airsoul/"
-export PYTHONPATH="$PYTHONPATH:/home/wangfan/cassel/airsoul/airsoul"
-python3 train.py config_test.yaml
